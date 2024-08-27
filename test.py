ac83ccbb97efc9e5c411f73dffb9076f7244035d
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2024-08-26 19:12:41 -0700

    uses euclidean distance between matrices

--- test.py
+++ test.py
@@ -4,51 +4,39 @@ from numpy.linalg import norm
 import numpy as np
 import pandas as pd
 from pymilvus import MilvusClient, connections, Collection
+from pymongo import MongoClient
+from scipy.spatial.distance import euclidean
+
+URI="mongodb+srv://kundurthisumedha:K2WQp4meOd8WFY5z@cluster0.pspbr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongodb_client = MongoClient(URI)
+database = mongodb_client["VoiceRec"]
+collection = database["voices"]
 
 
-connections.connect("default", host="localhost", port="19530")
-client = MilvusClient("voices.db")
+def pad_matrices(matrix1, matrix2):
+    rows1, cols1 = matrix1.shape
+    rows2, cols2 = matrix2.shape
+    max_rows = max(rows1, rows2)
+    padded_matrix1 = np.pad(matrix1, ((0, max_rows - rows1), (0, 0)), mode='constant')
+    padded_matrix2 = np.pad(matrix2, ((0, max_rows - rows2), (0, 0)), mode='constant')
 
+    return padded_matrix1, padded_matrix2
 
-def search(vector):
-    print("FLATTENED VECTOR")
-    print(vector)
-    res = client.search(
-    collection_name="Voice_Collection",  # target collection
-    data=[vector],  # query vectors
-    limit=10,  # number of returned entities
-    output_fields=["name"],  # specifies fields to be returned
-    )
-    return res
+def calculate_euclidean_distance(matrix1, matrix2):
+    flattened_matrix1 = matrix1.flatten()
+    flattened_matrix2 = matrix2.flatten()
+    distance = euclidean(flattened_matrix1, flattened_matrix2)
+    return distance
 
-
-
-
-
-
-# speaker=get_speaker(vectors)
-# print(speaker)
-# # ask user if correct, or if not, who?
-# ans=input("Is it correct (Y/N)?")
-# if ans=="Y":
-#     result = df[df['Name'] == speaker]
-#     current_vectors=result.iloc[0]['Voice-id']
-#     row_index = df[df['Name'] == speaker].index[0]
-#     v=[current_vectors]
-#     v.append(vectors)
-#     mean_v=np.mean(v, axis=0)
-#     print(mean_v)
-#     df.at[row_index, 'Voice-id'] = mean_v
-
-
-# elif ans=="N":
-#     ans=input("Correct Ans? ")
-#     result = df[df['Name'] == ans]
-#     current_vectors=result.iloc[0]['Voice-id']
-#     row_index = df[df['Name'] == ans].index[0]
-#     v=[current_vectors]
-#     v.append(vectors)
-#     mean_v=np.mean(v, axis=0)
-#     print(mean_v)
-#     df.at[row_index, 'Voice-id'] = mean_v
-# df.to_json("voices copy.json")
+def search(matrix):
+    min_distance=float('inf')
+    name=""
+    documents = collection.find()
+    for document in documents:
+        array = np.array(document["matrix"])
+        padded_matrix1,padded_matrix2=pad_matrices(matrix,array)
+        distance = calculate_euclidean_distance(padded_matrix1,padded_matrix2)
+        if distance<min_distance:
+            min_distance=distance
+            name=document["name"]   
+    return name
