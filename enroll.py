34da539d482feb9e63a45be13e635a74a93664f3
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2024-08-26 19:04:34 -0700

    used matrix over vector

--- enroll.py
+++ enroll.py
@@ -4,26 +4,26 @@ import numpy as np
 import json
 import pandas as pd
 from pymilvus import MilvusClient, CollectionSchema, FieldSchema, DataType, Collection, connections, utility
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-connections.connect("default", host="localhost", port="19530")
-client = MilvusClient("voices.db")
-colls=client.list_collections()
-print(colls)
+URI=os.getenv("MONGO_URL")
+mongodb_client = MongoClient(URI)
+database = mongodb_client["VoiceRec"]
+collection = database["voices"]
 name=input("Enter Name To Enroll: ")
-file_names=[]
-for i in range(1):
-    file_names.append(name+"/"+name+str(i))
-    record.record_audio(name+str(i))
-vectors=process.process_files(file_names)
-flat=vectors[0].flatten()[0:390]
-print(flat)
-i=client.get_collection_stats("Voice_Collection")["row_count"]
-data = [
-    {"id": i, "vector": flat, "name": name}
-]
 
-res = client.insert(collection_name="Voice_Collection", data=data)
-print(res)
+file_name=(name+"/"+name+"0")
+record.record_audio(name+str(0))
+vectors=process.process_file(file_name)
+print(vectors.shape)
+print(len(vectors.tolist()))
+print(len(vectors.tolist()[0]))
+obj={"matrix":vectors.tolist(), "name":name}
+collection.insert_one(obj)
+mongodb_client.close()
 
 
 
